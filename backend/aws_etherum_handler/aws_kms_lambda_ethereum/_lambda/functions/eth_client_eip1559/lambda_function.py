bd4ae363949101380856069b5a15af62f5a7e47e
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2023-02-01 22:45:03 +0900

    初期設定

---
+++ backend/aws_etherum_handler/aws_kms_lambda_ethereum/_lambda/functions/eth_client_eip1559/lambda_function.py
@@ -0,0 +1,155 @@
+#  SPDX-License-Identifier: MIT-0
+import json
+import boto3
+import logging
+import os
+
+from lambda_helper import (assemble_tx,
+                           assemble_contract,
+                           get_params,
+                           get_tx_params,
+                           get_contract_params,
+                           calc_eth_address,
+                           get_kms_public_key)
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING")
+LOG_FORMAT = "%(levelname)s:%(lineno)s:%(message)s"
+handler = logging.StreamHandler()
+
+_logger = logging.getLogger()
+_logger.setLevel(LOG_LEVEL)
+
+'''
+Lambda関数実行時の引数によって、実行する処理の内容を変える
+- status -> 所定のKMS IDの秘密鍵が持つウォレットアドレスを返す
+- send -> 所定のKMS IDの秘密鍵を使用して、送金トランザクションを実行する
+'''
+def lambda_handler(event, context):
+    _logger.debug("incoming event: {}".format(event))
+
+    try:
+        params = get_params()
+    except Exception as e:
+        raise e
+
+    operation = event.get('operation')
+    if not operation:
+        raise ValueError('operation needs to be specified in request and needs to be eigher "status" or "send"')
+
+    # {"operation": "status"}
+    if operation == 'status':
+        key_id = os.getenv('KMS_KEY_ID')
+        pub_key = get_kms_public_key(key_id)
+        eth_checksum_address = calc_eth_address(pub_key)
+
+        return {'eth_checksum_address': eth_checksum_address}
+
+    # 送金トランザクション ※引数のイメージは下記
+    # {"operation": "send",
+    #  "amount": 123,
+    #  "dst_address": "0x...",
+    #  "nonce": 0}
+    elif operation == 'send':
+
+        if not (event.get('dst_address') and event.get('amount', -1) >= 0 and event.get('nonce', -1) >= 0):
+            return {
+                'operation': 'sign',
+                'error': 'missing parameter - sign requires amount, dst_address and nonce to be specified'
+            }
+
+        # Set Params from environment varaible
+        key_id = os.getenv('KMS_KEY_ID')
+
+        # Set Params from send request
+        dst_address = event.get('dst_address')
+        amount = event.get('amount')
+        chainid = event.get('chainid')
+        type = event.get('type')
+        max_fee_per_gas = event.get('max_fee_per_gas')
+        max_priority_fee_per_gas = event.get('max_priority_fee_per_gas')
+
+        # download public key from KMS & calculate the Ethereum public address
+        pub_key = get_kms_public_key(key_id)
+        eth_checksum_addr = calc_eth_address(pub_key)
+
+        # collect rawd parameters for Ethereum transaction
+        tx_params = get_tx_params(dst_address=dst_address,
+                                  amount=amount,
+                                  eth_addr=eth_checksum_addr,
+                                  chainid=chainid,
+                                  type=type,
+                                  max_fee_per_gas=max_fee_per_gas,
+                                  max_priority_fee_per_gas=max_priority_fee_per_gas)
+
+        # assemble Ethereum transaction and sign it offline
+        raw_tx_signed_hash, raw_tx_signed_payload = assemble_tx(tx_params=tx_params,
+                                                                params=params,
+                                                                eth_checksum_addr=eth_checksum_addr,
+                                                                chainid=chainid)
+
+        return {
+            'operation': 'sign',
+            "signed_tx_hash": raw_tx_signed_hash,
+            "signed_tx_payload": raw_tx_signed_payload
+        }
+
+    # ToDo：スマートコントラクトの実行
+    elif operation == 'nft721':
+        if not (event.get('max_fee_per_gas', -1) >= 0):
+            return {
+                'operation': 'nft721',
+                'error': 'missing parameter - NFT721 requires max_fee_per_gas to be specified'
+            }
+        
+        s3 = boto3.client('s3')
+        
+        # Set Params from environment varaible
+        key_id = os.getenv('KMS_KEY_ID')
+
+        # Get Contract JSON from S3
+        s3_bucket = event.get('contract_json_bucket')
+        s3_obj_key = event.get('contract_json_objkey')
+        s3_res = s3.get_object(Bucket=s3_bucket, Key=s3_obj_key)
+        contract_json = json.load(s3_res['Body'])
+
+        # Set Params from send request
+        dst_address = event.get('dst_address')
+        chainid = event.get('chainid')
+        type = event.get('type')
+        gas = event.get('gas')
+        max_fee_per_gas = event.get('max_fee_per_gas')
+        max_priority_fee_per_gas = event.get('max_priority_fee_per_gas')
+        contract_addr = event.get('contract_address')
+        contract_func = event.get ('contract_function')
+        contract_params = event.get('contract_params')
+
+        # download public key from KMS & calculate the Ethereum public address
+        pub_key = get_kms_public_key(key_id)
+        eth_checksum_addr = calc_eth_address(pub_key)
+
+        # collect rawd parameters for Ethereum transaction
+        tx_params = get_contract_params(eth_addr=eth_checksum_addr,
+                                  chainid=chainid,
+                                  type=type,
+                                  gas=gas,
+                                  max_fee_per_gas=max_fee_per_gas,
+                                  max_priority_fee_per_gas=max_priority_fee_per_gas)
+
+        # assemble Ethereum transaction and sign it offline
+        tx_hash = assemble_contract(tx_params=tx_params,
+                                    params=params,
+                                    contract_json=contract_json,
+                                    contract_addr=contract_addr,
+                                    contract_func=contract_func,
+                                    contract_params=contract_params)
+
+        return {
+            'operation': 'nft721',
+            "signed_tx_hash": tx_hash
+        }
+
+    # ToDO：BlockcertsによるVC発行
+    elif operation == 'issuer':
+        return {
+            "message": "Hello, World!!"
+        }
---
+++ backend/aws_etherum_handler/aws_kms_lambda_ethereum/_lambda/functions/eth_client_eip1559/lambda_helper.py
@@ -0,0 +1,247 @@
+#  SPDX-License-Identifier: MIT-0
+
+import logging
+import os
+import sys
+
+import asn1tools
+import boto3
+from ethereum_kms_signer import sign_transaction
+from eth_account import Account
+from eth_account._utils.signing import (
+    encode_transaction, serializable_unsigned_transaction_from_dict)
+#from web3.auto import w3
+from web3 import Web3, HTTPProvider
+from web3.contract import ConciseContract
+provider = os.getenv('ETH_PROVIDER')
+w3 = Web3(HTTPProvider(provider))
+
+session = boto3.session.Session()
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(message)s')
+handler.setFormatter(formatter)
+
+_logger = logging.getLogger('app')
+_logger.setLevel(os.getenv('LOGGING_LEVEL', 'WARNING'))
+_logger.addHandler(handler)
+
+# max value on curve / https://github.com/ethereum/EIPs/blob/master/EIPS/eip-2.md
+SECP256_K1_N = int("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
+
+
+class EthKmsParams:
+
+    def __init__(self, kms_key_id: str, eth_network: str):
+        self._kms_key_id = kms_key_id
+        self._eth_network = eth_network
+
+    def get_kms_key_id(self) -> str:
+        return self._kms_key_id
+
+
+def get_params() -> EthKmsParams:
+    for param in ['KMS_KEY_ID', 'ETH_NETWORK']:
+        value = os.getenv(param)
+
+        if not value:
+            if param in ['ETH_NETWORK']:
+                continue
+            else:
+                raise ValueError('missing value for parameter: {}'.format(param))
+
+    return EthKmsParams(
+        kms_key_id=os.getenv('KMS_KEY_ID'),
+        eth_network=os.getenv('ETH_NETWORK')
+    )
+
+
+def get_kms_public_key(key_id: str) -> bytes:
+    client = boto3.client('kms')
+
+    response = client.get_public_key(
+        KeyId=key_id
+    )
+
+    return response['PublicKey']
+
+
+def sign_kms(key_id: str, msg_hash: bytes) -> dict:
+    client = boto3.client('kms')
+
+    response = client.sign(
+        KeyId=key_id,
+        Message=msg_hash,
+        MessageType='DIGEST',
+        SigningAlgorithm='ECDSA_SHA_256'
+    )
+
+    return response
+
+
+def calc_eth_address(pub_key) -> str:
+    SUBJECT_ASN = '''
+    Key DEFINITIONS ::= BEGIN
+
+    SubjectPublicKeyInfo  ::=  SEQUENCE  {
+       algorithm         AlgorithmIdentifier,
+       subjectPublicKey  BIT STRING
+     }
+
+    AlgorithmIdentifier  ::=  SEQUENCE  {
+        algorithm   OBJECT IDENTIFIER,
+        parameters  ANY DEFINED BY algorithm OPTIONAL
+      }
+
+    END
+    '''
+
+    key = asn1tools.compile_string(SUBJECT_ASN)
+    key_decoded = key.decode('SubjectPublicKeyInfo', pub_key)
+
+    pub_key_raw = key_decoded['subjectPublicKey'][0]
+    pub_key = pub_key_raw[1:len(pub_key_raw)]
+
+    # https://www.oreilly.com/library/view/mastering-ethereum/9781491971932/ch04.html
+    hex_address = w3.keccak(bytes(pub_key)).hex()
+    eth_address = '0x{}'.format(hex_address[-40:])
+
+    eth_checksum_addr = w3.toChecksumAddress(eth_address)
+
+    return eth_checksum_addr
+
+
+def find_eth_signature(params: EthKmsParams, plaintext: bytes) -> dict:
+    SIGNATURE_ASN = '''
+    Signature DEFINITIONS ::= BEGIN
+
+    Ecdsa-Sig-Value  ::=  SEQUENCE  {
+           r     INTEGER,
+           s     INTEGER  }
+
+    END
+    '''
+    signature_schema = asn1tools.compile_string(SIGNATURE_ASN)
+
+    signature = sign_kms(params.get_kms_key_id(), plaintext)
+    print("KMS signature ↓↓↓↓↓↓↓:")
+    print(signature)
+    # https://tools.ietf.org/html/rfc3279#section-2.2.3
+    signature_decoded = signature_schema.decode('Ecdsa-Sig-Value', signature['Signature'])
+    s = signature_decoded['s']
+    r = signature_decoded['r']
+
+    secp256_k1_n_half = SECP256_K1_N / 2
+
+    if s > secp256_k1_n_half:
+        s = SECP256_K1_N - s
+
+    return {'r': r, 's': s}
+
+
+def get_recovery_id(msg_hash, r, s, eth_checksum_addr, chainid) -> dict:
+    # デジタル署名の vを計算する(EIP155) ※https://eips.ethereum.org/EIPS/eip-155
+    # {0,1} + CHAIN_ID * 2 + 35
+    v_lower = chainid * 2 + 35
+    v_range = [v_lower, v_lower + 1]
+    for v in v_range:
+        recovered_addr = Account.recoverHash(message_hash=msg_hash, vrs=(v, r, s))
+        if recovered_addr == eth_checksum_addr:
+            return {"recovered_addr": recovered_addr, "y_parity": v - v_lower}
+    return {}
+
+def get_tx_params(dst_address: str, 
+                  amount: int, 
+                  eth_addr: str,
+                  chainid: int, 
+                  type: int, 
+                  gas: int,
+                  max_fee_per_gas: int, 
+                  max_priority_fee_per_gas: int) -> dict:
+    
+    nonce = w3.eth.getTransactionCount(eth_addr)
+    transaction = {
+        'nonce': nonce,
+        'to': dst_address,
+        'value': w3.toWei(amount, 'ether'),
+        'data': '0x00',
+        'maxFeePerGas': max_fee_per_gas,
+        'maxPriorityFeePerGas': max_priority_fee_per_gas,
+        'type': type,
+        'chainId': chainid,
+    }
+    return transaction
+
+def get_contract_params(eth_addr: str,
+                        chainid: int, 
+                        type: int, 
+                        gas: int,
+                        max_fee_per_gas: int, 
+                        max_priority_fee_per_gas: int) -> dict:
+    nonce = w3.eth.getTransactionCount(eth_addr)
+    transaction = {
+        'nonce': nonce,
+        'gas': gas,
+        'maxFeePerGas': max_fee_per_gas,
+        'maxPriorityFeePerGas': max_priority_fee_per_gas,
+        'type': type,
+        'chainId': chainid,
+    }
+
+    return transaction
+
+
+def assemble_tx(tx_params: dict, 
+                params: EthKmsParams, 
+                eth_checksum_addr: str, 
+                chainid: int) -> (bytes, bytes):
+    tx_unsigned = serializable_unsigned_transaction_from_dict(transaction_dict=tx_params)
+    tx_hash = tx_unsigned.hash()
+
+    #渡されたtx_paramsではdataが仮の値のため、署名した値を代入する
+    #tx_sig : KMSで作成したデジタル署名のr, s
+    tx_sig = find_eth_signature(params=params,plaintext=tx_hash)
+    tx_eth_recovered_pub_addr = get_recovery_id(msg_hash=tx_hash,
+                                                r=tx_sig['r'],
+                                                s=tx_sig['s'],
+                                                eth_checksum_addr=eth_checksum_addr,
+                                                chainid=chainid)
+    tx_encoded = encode_transaction(unsigned_transaction=tx_unsigned,
+                                    vrs=(
+                                        tx_eth_recovered_pub_addr['y_parity'], 
+                                        tx_sig['r'], 
+                                        tx_sig['s']
+                                    ))
+    tx_params['data'] = tx_encoded
+
+    # Signing the transaction with KMS key
+    tx_singed = sign_transaction(tx_params, params.get_kms_key_id())
+
+    # send transaction
+    tx_hash = w3.eth.sendRawTransaction(tx_singed.rawTransaction)
+    tx_encoded_hex = w3.toHex(tx_encoded)
+    tx_hash = w3.keccak(hexstr=tx_encoded_hex).hex()
+    print('translate tx information!!!!!')
+    return tx_hash, tx_hash
+
+def assemble_contract(tx_params: dict, 
+                      params: EthKmsParams, 
+                      contract_json: dict, 
+                      contract_addr: str, 
+                      contract_func: str,
+                      contract_params: dict) -> (bytes):
+    abi = contract_json['abi']
+    contract_attr_checked = Web3.toChecksumAddress(contract_addr)
+    contract_instance = w3.eth.contract(abi=abi, address=contract_attr_checked)
+
+    # assemble function in constract
+    print(contract_params)
+    constract_func_checked = contract_instance.functions[contract_func](
+        contract_params["recipient"],
+        contract_params["tokenURI"],
+        contract_params["id"])
+    tx_unsigned = constract_func_checked.buildTransaction(tx_params)
+
+    tx_singed = sign_transaction(tx_unsigned, params.get_kms_key_id())
+    tx_hash = w3.eth.sendRawTransaction(tx_singed.rawTransaction)
+
+    return tx_hash
