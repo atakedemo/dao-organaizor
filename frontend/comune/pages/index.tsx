f913e3db3b6d4a742b77d501872579b2b864029a
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2023-03-21 21:26:33 +0900

    フロント土台作成

---
+++ frontend/comune/pages/index.tsx
@@ -0,0 +1,22 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          {/* 自分の作成する DAO 名に変更してください */}
+          Comune 
+        </h1>
+        <p></p>
+        <div className={styles.connect}>
+          <ConnectWallet />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
